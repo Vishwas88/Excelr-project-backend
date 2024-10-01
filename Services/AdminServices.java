9f6e65b8d7d42c02ce975b61a7cb3656b1922827
naga-bhargavi <84374071+naga-bhargavi@users.noreply.github.com>
2024-10-01 22:05:48 +0530

    Create AdminServices.java

---
+++ Services/AdminServices.java
@@ -0,0 +1,21 @@
+package com.excelr.finalproject.EmployeeManagementProject.Services;
+
+import java.util.List;
+
+
+
+import com.excelr.finalproject.EmployeeManagementProject.AdminDetails.AdminDetails;
+
+
+public interface AdminServices {
+	public AdminDetails saveAdmin(AdminDetails add);
+	public List<AdminDetails> getAllAdmins(AdminDetails admin);
+	
+	
+	
+	
+	
+	
+
+	
+}
